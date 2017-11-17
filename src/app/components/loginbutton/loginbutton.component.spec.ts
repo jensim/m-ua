31c651c611222b1750d2bafa86cb72211ba41f96
jensim / Jens Brimfors <jens.brimfors@gmail.com>
2017-11-17 05:26:56 +0100

    Added login component

---
+++ src/app/components/loginbutton/loginbutton.component.spec.ts
@@ -0,0 +1,8 @@
+import { LoginButtonComponent } from './loginbutton.component';
+
+describe('LoginbuttonDirective', () => {
+  it('should create an instance', () => {
+    const directive = new LoginButtonComponent();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/components/loginbutton/loginbutton.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import * as firebase from 'firebase/app';
+
+@Component({
+  selector: 'app-login-button',
+  templateUrl: './loginbutton.component.html',
+  styleUrls: ['./loginbutton.component.css']
+})
+export class LoginButtonComponent {
+
+  constructor() { }
+}
